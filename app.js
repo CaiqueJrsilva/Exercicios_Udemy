8ed6c4a7dafe8c0b99baf3710599fcb18d17df98
Caique Junior da silva <139816879+CaiqueJrsilva@users.noreply.github.com>
2023-09-15 14:39:38 -0300

    atualizado o projeto

---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("Rodando o gerenciamento dos exercicios ")
+console.log("Trocado o nome do arquivo e criado um arquivo em python")
--- exercicios.py
+++
@@ -1,1 +0,0 @@
-console.log("Rodando o gerenciamento dos exercicios ")
