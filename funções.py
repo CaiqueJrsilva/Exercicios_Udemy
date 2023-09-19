db36a55e277bbabc0e0cd6979c5bb9039d4b410b
Caique Junior da silva <139816879+CaiqueJrsilva@users.noreply.github.com>
2023-09-19 13:02:56 -0300

    funçoes

--- funções.py
+++ funções.py
@@ -1,9 +1,40 @@
-def multiplo_de(numero, multiplo):
-    resultado = numero % multiplo == 0
-    print(f'{numero} é múltiplo de {multiplo}?', end=' ')
-    print(resultado)
+# def multiplo_de(numero, multiplo):
+#     resultado = numero % multiplo == 0
+#     print(f'{numero} é múltiplo de {multiplo}?', end=' ')
+#     print(resultado)
  
  
-multiplo_de(16, 8)
-multiplo_de(15, 3)
-multiplo_de(10, 2)
+# multiplo_de(16, 8)
+# multiplo_de(15, 3)
+# multiplo_de(10, 2)
+
+
+# def soma(x , y, z ):
+#     print(x + y + z)
+
+
+# soma(1,2)
+# soma(2,5)
+# soma(7,9)
+
+
+# x=1
+
+# def escopo():
+#         x=11
+
+#         def outra_funncao():
+#             x = 5
+#             y = 7
+#             print(x,y)
+
+#         outra_funncao()
+#         print(x)
+
+# print(x)
+# escopo()
+# print(x)
+
+
+
+
